00a429e043cbb47b3887fc21a661bdad2ce70486
starukhin <vasyl.starukhin@onedome.com>
2021-02-15 01:09:05 +0200

    lightweight webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
+
+module.exports = (env) => {
+    return {
+        context: __dirname,
+        entry: './src/index.js',
+        output: {
+            path: path.resolve( __dirname, 'dist' ),
+            filename: 'main.js',
+            publicPath: '/',
+        },
+        resolve: {
+            extensions: ['.js', '.jsx'],
+            alias: {
+                src: path.resolve('./src')
+            }
+        },
+        devServer: {
+            historyApiFallback: true,
+            open: true,
+            port: env.port || 3000
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx)$/,
+                    use: 'babel-loader',
+                },
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader'],
+                },
+                {
+                    test: /\.(png|j?g|svg|gif)?$/,
+                    use: 'file-loader'
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                template: path.resolve( __dirname, 'public/index.html' ),
+                filename: 'index.html'
+            })
+        ]
+    }
+};
